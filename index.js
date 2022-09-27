cca01723026a0d057cd84f7eeb1f2f7b56dae981
carine ferreira da silva <carine.silvaf1@gmail.com>
2022-09-27 15:44:18 -0300

    commit tree 63ddc7c02c543191fe8becc29b96ad3b3d6b2c2e

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 62, num2 = 1
+const multiply = num1 * num2
+
+const random = parseInt(Math.random()*10)
+
+const num3 = 24, num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(2,3,5,20,14,9)
